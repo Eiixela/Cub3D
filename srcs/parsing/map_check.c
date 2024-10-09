24373f7e9686c9ce0cc474343d96c3b6b1b2d9ae
Alexie Julien <aljulien@z1r3p1.42lyon.fr>
2024-10-09 15:20:45 +0200

    added basic flood_fill

--- srcs/parsing/map_check.c
+++ srcs/parsing/map_check.c
@@ -6,11 +6,34 @@
 /*   By: aljulien <aljulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 09:39:15 by aljulien          #+#    #+#             */
-/*   Updated: 2024/10/09 15:06:16 by aljulien         ###   ########.fr       */
+/*   Updated: 2024/10/09 15:19:24 by aljulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
+
+/*//Basic flood_fill function 
+typedef struct	s_point
+{
+	int x;
+	int y;
+} t_point;
+
+void	fill(char **tab, t_point size, t_point cur, char to_fill)
+{
+	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x || tab[cur.y][cur.x] != to_fill)
+		return;
+	tab[cur.y][cur.x] = 'F';
+	fill(tab, size, (t_point){cur.x - 1, cur.y}, to_fill);
+	fill(tab, size, (t_point){cur.x + 1, cur.y}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y - 1}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y + 1}, to_fill);
+}
+
+void	flood_fill(char **tab, t_point size, t_point begin)
+{
+	fill(tab, size, begin, tab[begin.y][begin.x]);
+}*/
 
 int	player_way(t_map **map)
 {
