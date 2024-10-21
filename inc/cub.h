0b764abb5a9845efc702720ad55883350c9fdec6
Alexie Julien <aljulien@z1r3p5.42lyon.fr>
2024-10-21 14:55:07 +0200

    added define lol

--- inc/cub.h
+++ inc/cub.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saperrie <saperrie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aljulien <aljulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 09:13:52 by aljulien          #+#    #+#             */
-/*   Updated: 2024/10/21 14:22:27 by saperrie         ###   ########.fr       */
+/*   Updated: 2024/10/21 14:53:16 by aljulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@
 # define WIDTH 2500
 # define HEIGHT 1300
 # define PI	3.1415926535
+# define PLAYER_SIZE 10
+# define SQUARE_SIZE 64
 
 //--------------------------------------------------|
 
--- srcs/sam/draw.c
+++ srcs/sam/draw.c
@@ -15,9 +15,9 @@
 int	player_position(double new_x, double new_y, t_map *map)
 {
 	(void)map;
-	double player_y_middle = map->player_position->y * 64 + 64 / 2;
-	double player_x_middle = map->player_position->x * 64 + 64 / 2;
-	if ((new_y <=(player_y_middle + 10/2) && new_y >= (player_y_middle - 10/2) && new_x <= (player_x_middle + 10/2) && new_x >= (player_x_middle - 10/2)))
+	double player_y_middle = map->player_position->y * SQUARE_SIZE + SQUARE_SIZE / 2;
+	double player_x_middle = map->player_position->x * SQUARE_SIZE + SQUARE_SIZE / 2;
+	if ((new_y <=(player_y_middle + PLAYER_SIZE/2) && new_y >= (player_y_middle - PLAYER_SIZE/2) && new_x <= (player_x_middle + PLAYER_SIZE/2) && new_x >= (player_x_middle - PLAYER_SIZE/2)))
 		return (0);
 	return (1);
 	// if (new_x == map->player_position->x && new_y == map->player_position->y)
@@ -30,7 +30,6 @@ int draw_new_2d(t_map *map, t_data *data)
 {
 	char	*pixels;
 	double	 draw_x, draw_y, x, y, new_y, new_x, width, height;
-	int	 square_size = 64; // Size of each square
 
 	width = map->size->y;
 	height = map->size->x;
@@ -38,13 +37,13 @@ int draw_new_2d(t_map *map, t_data *data)
 	y = 0;
 	while (y < HEIGHT)
 	{
-		new_y = y / square_size;
+		new_y = y / SQUARE_SIZE;
 		draw_y = y * WIDTH;
 		x = 0;
 		while (x < WIDTH)
 		{
 			draw_x = x;
-			new_x = x / square_size;
+			new_x = x / SQUARE_SIZE;
 			if (new_x < width && new_y < height)
 			{
 				char	current_char = map->map[(int)new_y][(int)new_x];
@@ -59,7 +58,7 @@ int draw_new_2d(t_map *map, t_data *data)
 					color = 0;
 				((int *)pixels)[(int)draw_x + (int)draw_y] = color;
 				if ((current_char == '0' || current_char == '1') && \
-					((int)x % square_size == 0 || (int)y % square_size == 0))
+					((int)x % SQUARE_SIZE == 0 || (int)y % SQUARE_SIZE == 0))
 					((int *)pixels)[(int)draw_x + (int)draw_y] = GRID_COLOR;
 			}
 			x++;
